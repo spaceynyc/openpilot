57c2bb096d2daf7e0417c3c27ccc4209c702f8cf
nrdr <81265253+nrdr@users.noreply.github.com>
2026-04-27 21:56:14 -0400

    Update interface.py

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -362,7 +362,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.HONDA_CLARITY:
       stock_cp.autoResumeSng = True
       stock_cp.minEnableSpeed = -1
-      stock_cp.steerActuatorDelay = 0.30
+      stock_cp.steerActuatorDelay = 1.0
       stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 1663], [0, 1663]]
       stock_cp.lateralTuning.pid.kf = 0.00006
       stock_cp.lateralTuning.pid.kpBP, stock_cp.lateralTuning.pid.kpV = [[0, 10, 20], [0.3, 0.3, 0.3]]
