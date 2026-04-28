0ec550a43998678e0f2b3e7ef58b4bb912002842
nrdr <81265253+nrdr@users.noreply.github.com>
2026-04-27 21:13:33 -0400

    Try to match Clarity fw response

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -337,8 +337,10 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in (CAR.HONDA_CIVIC_BOSCH, CAR.HONDA_CIVIC_BOSCH_DIESEL):
       if ret.flags & HondaFlagsSP.EPS_MODIFIED:
-        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]]
-        CarInterfaceBase.configure_torque_tune(candidate, stock_cp.lateralTuning)
+        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 1663], [0, 1663]]
+        stock_cp.lateralTuning.pid.kf = 0.00006
+        stock_cp.lateralTuning.pid.kpBP, stock_cp.lateralTuning.pid.kpV = [[0, 10, 20], [0.3, 0.3, 0.3]]
+        stock_cp.lateralTuning.pid.kiBP, stock_cp.lateralTuning.pid.kiV = [[0, 10, 20], [0.1, 0.1, 0.1]]
 
     elif candidate == CAR.HONDA_CIVIC_2022:
       if ret.flags & HondaFlagsSP.EPS_MODIFIED:
@@ -360,8 +362,11 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.HONDA_CLARITY:
       stock_cp.autoResumeSng = True
       stock_cp.minEnableSpeed = -1
-      stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]]
-      CarInterfaceBase.configure_torque_tune(candidate, stock_cp.lateralTuning)
+      stock_cp.steerActuatorDelay = 0.30
+      stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 1663], [0, 1663]]
+      stock_cp.lateralTuning.pid.kf = 0.00006
+      stock_cp.lateralTuning.pid.kpBP, stock_cp.lateralTuning.pid.kpV = [[0, 10, 20], [0.3, 0.3, 0.3]]
+      stock_cp.lateralTuning.pid.kiBP, stock_cp.lateralTuning.pid.kiV = [[0, 10, 20], [0.1, 0.1, 0.1]]
 
     elif candidate in (CAR.ACURA_MDX_3G, CAR.ACURA_MDX_3G_MMR):  # source mlocoteta
       stock_cp.autoResumeSng = True
--- opendbc_repo/opendbc/car/honda/values.py
+++ opendbc_repo/opendbc/car/honda/values.py
@@ -36,8 +36,8 @@ class CarControllerParams:
   BOSCH_GAS_LOOKUP_V = [0, 1600]
 
   STEER_STEP = 1  # 100 Hz
-  STEER_DELTA_UP = 10000
-  STEER_DELTA_DOWN = 10000
+  STEER_DELTA_UP = 35
+  STEER_DELTA_DOWN = 58
   STEER_GLOBAL_MIN_SPEED = 0 * CV.MPH_TO_MS
 
   def __init__(self, CP):
