fa0263df7734eb89f1c4525c74164b87d27d1db0
spaceynyc <srich7x@gmail.com>
2026-06-12 06:41:01 -0400

    Create stock-based Insight smooth long test branch

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -70,15 +70,19 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
     raise NotImplementedError("Longitudinal personality not supported")
 
 
-def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
+def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard, v_ego=None):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 1.75
+    t_follow = 1.75
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.45
+    t_follow = 1.45
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.25
+    t_follow = 1.25
   else:
     raise NotImplementedError("Longitudinal personality not supported")
+
+  if v_ego is not None:
+    t_follow += np.interp(v_ego, [0.0, 15.0], [0.25, 0.0])
+  return t_follow
 
 def get_stopped_equivalence_factor(v_lead):
   return (v_lead**2) / (2 * COMFORT_BRAKE)
@@ -314,8 +318,8 @@ class LongitudinalMpc:
     return lead_xv
 
   def update(self, radarstate, v_cruise, personality=log.LongitudinalPersonality.standard):
-    t_follow = get_T_FOLLOW(personality)
     v_ego = self.x0[1]
+    t_follow = get_T_FOLLOW(personality, v_ego=v_ego)
     self.status = radarstate.leadOne.status or radarstate.leadTwo.status
 
     lead_xv_0 = self.process_lead(radarstate.leadOne)
